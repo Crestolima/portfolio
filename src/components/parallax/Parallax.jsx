a9b02f555b871b83298462d93cedfda2fecf1317
Cresto Lima <crestolima67@gmail.com>
2024-02-03 18:55:35 +0530

    paralax

--- src/components/parallax/Parallax.jsx
+++ src/components/parallax/Parallax.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
 import "./parallax.scss";
+import { motion, useScroll, useTransform } from 'framer-motion';
+import  { useRef  } from 'react';
 
 function Parallax({ type }) {
+
+  // const ref = useRef()
+
+  // const {scrollyProgress} = useScroll({
+  //   target:ref,
+  //   offset: ["start start","end start"]
+  // })
+  // const ytext = useTransform(scrollyProgress, [0,1], ["0%", "500%"])
+  // const ybg = useTransform(scrollyProgress, [0,1], ["0%", "100%"])
+
   return (
-    <div className='parallax' style={{ background:
+    <div className='parallax'   style={{ background:
         type === "Technical Skills" ? "linear-gradient(180deg, #111132, #0c0c1d)" : "linear-gradient(180deg, #111132, #505064)"
     }}>
-      <h1>{type === "Technical Skills" ? "What I Know?" : "What I Did?"}</h1>
-      <div className="mountains"><img src='./mountains.png' alt="mountains" /></div>
-      <div className="planets"><img src='./planets.png' alt="planets" /></div>
-      <div className="stars"><img src='./stars.png' alt="stars" /></div>
+      <h1 >{type === "Technical Skills" ? "What I Know?" : "What I Did?"}</h1>
+      <div className="mountains"></div>
+      <div  className="planets"></div>
+      <div  className="stars"></div>
     </div>
   );
 }
