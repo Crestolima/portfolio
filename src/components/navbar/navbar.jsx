1d8891580ecabc09b47fcb810095cf7bd2b4d7c6
Cresto Lima <crestolima67@gmail.com>
2024-10-26 13:03:38 +0530

    Made SVG scale properly with its container

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,29 +1,52 @@
 import React from 'react';
-import "./navbar.scss";
-import { motion } from "framer-motion";
+import './navbar.scss';
+import { motion } from 'framer-motion';
 import Sidebar from '../sidebar/Links/Sidebar';
 
-
-
 export default function Navbar() {
-    return (
-        <div className='navbar'>
-            
-            <Sidebar />
-            <div className="wrapper">
-                <motion.span
-                    initial={{ opacity: 0, scale: 0.5 }}
-                    animate={{ opacity: 1, scale: 1 }}
-                    transition={{ duration: 0.5 }} className="name">
-                    Portfolio 
-                </motion.span>
-                <div className="social">
-                    <a href='#'><img src="/facebook.png" alt="Fb" /></a>
-                    <a href='#'><img src="/instagram.png" alt="Insta" /></a>
-                    <a href='#'><img src="/youtube.png" alt="Yt" /></a>
-                    <a href='#'><img src="/dribbble.png" alt="Db" /></a>
-                </div>
-            </div>
+  return (
+    <nav className="navbar">
+      <div className="navbar-wrapper">
+        <Sidebar />
+        <motion.span
+          className="logo"
+          initial={{ opacity: 0, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5 }}
+        >
+          Portfolio
+        </motion.span>
+        <div className="social-links">
+          <motion.a 
+            href="#" 
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+          >
+            <img src="/facebook.png" alt="Facebook" />
+          </motion.a>
+          <motion.a 
+            href="#" 
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+          >
+            <img src="/instagram.png" alt="Instagram" />
+          </motion.a>
+          <motion.a 
+            href="#" 
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+          >
+            <img src="/youtube.png" alt="YouTube" />
+          </motion.a>
+          <motion.a 
+            href="#" 
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+          >
+            <img src="/dribbble.png" alt="Dribbble" />
+          </motion.a>
         </div>
-    )
+      </div>
+    </nav>
+  );
 }
